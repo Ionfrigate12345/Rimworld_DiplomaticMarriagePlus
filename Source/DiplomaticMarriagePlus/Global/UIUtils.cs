8f5a92a7f10f1271b6cdf747bd167ea36ad2d2d6
Ionfrigate12345 <alfred.shi0404@gmail.com>
2024-06-29 15:08:36 -0400

    Add widget for PA and temporary stay

---
+++ Source/DiplomaticMarriagePlus/Global/UIUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiplomaticMarriagePlus.Model;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace DiplomaticMarriagePlus.Global
+{
+    internal class UIUtils
+    {
+        public static void AddWidget(ref float curBaseY, TaggedString title, TaggedString description)
+        {
+            var rightMargin = 7f;
+            var zlRect = new Rect(UI.screenWidth - Alert.Width, curBaseY - 24f, Alert.Width, 24f);
+            Text.Font = GameFont.Small;
+
+            if (Mouse.IsOver(zlRect))
+            {
+                Widgets.DrawHighlight(zlRect);
+            }
+
+            GUI.BeginGroup(zlRect);
+            Text.Anchor = TextAnchor.UpperRight;
+            var rect = zlRect.AtZero();
+            rect.xMax -= rightMargin;
+
+            Widgets.Label(rect, title);
+            Text.Anchor = TextAnchor.UpperLeft;
+            GUI.EndGroup();
+
+            TooltipHandler.TipRegion(zlRect, new TipSignal(description));
+
+            curBaseY -= zlRect.height;
+        }
+    }
+}
--- Source/DiplomaticMarriagePlus/HarmonyPatches.cs
+++ Source/DiplomaticMarriagePlus/HarmonyPatches.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
+using DiplomaticMarriagePlus.Global;
+using DiplomaticMarriagePlus.Model;
 using HarmonyLib;
 using RimWorld;
+using UnityEngine;
 using Verse;
+using static UnityEngine.GraphicsBuffer;
 
 namespace DiplomaticMarriagePlus
 {
@@ -18,6 +22,52 @@ namespace DiplomaticMarriagePlus
         {
             _harmonyInstance = new Harmony("com.ionfrigate12345.diplomaticmarriageplus");
             _harmonyInstance.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(GlobalControlsUtility), nameof(GlobalControlsUtility.DoDate))]
+    internal class ShowTemporaryStayRemaining
+    {
+        private static void Postfix(ref float curBaseY)
+        {
+            var permanentAlliance = Find.World.GetComponent<PermanentAlliance>();
+            var temporaryStay = Find.World.GetComponent<TemporaryStay>();
+
+            //如果有有效的永久同盟，则显示状态
+            if(permanentAlliance != null && permanentAlliance.IsValid() == PermanentAlliance.Validity.VALID)
+            {
+                UIUtils.AddWidget(ref curBaseY,
+                    "DMP_PermanentAllianceInfo_WidgetTitle".Translate(),
+                    TranslatorFormattedStringExtensions.Translate("DMP_PermanentAllianceInfo_WidgetTitleDesc",
+                        NamedArgumentUtility.Named(permanentAlliance.PlayerBetrothed.Name, "{0}"),
+                        NamedArgumentUtility.Named(permanentAlliance.NpcMarriageSeeker.Name, "{1}"),
+                        NamedArgumentUtility.Named(permanentAlliance.WithFaction.Name, "{2}")
+                    )
+                );
+            }
+
+            //显示目前的夫妇暂住信息
+            if (temporaryStay != null && temporaryStay.IsRunning && temporaryStay.IsCurrentlyOnVisit)
+            {
+                //夫妇已抵达，正在暂住中
+                var ticksRemaining = temporaryStay.TickLastTemporaryVisitEnd - GenTicks.TicksAbs;
+                var hoursRemaining = CalculateRemainingHours(ticksRemaining);
+
+                UIUtils.AddWidget(ref curBaseY, 
+                    "DMP_TemporaryStay_WidgetTitle".Translate(), 
+                    TranslatorFormattedStringExtensions.Translate("DMP_TemporaryStay_WidgetTitleDesc",
+                        NamedArgumentUtility.Named(permanentAlliance.WithFaction.Name, "{0}"),
+                        NamedArgumentUtility.Named(permanentAlliance.PlayerBetrothed.Name, "{1}"),
+                        NamedArgumentUtility.Named(permanentAlliance.NpcMarriageSeeker.Name, "{2}"),
+                        NamedArgumentUtility.Named(Math.Round(hoursRemaining / 24.0f, 1), "{3}")
+                    )
+                );
+            }
+        }
+
+        private static float CalculateRemainingHours(float ticksRemaining)
+        {
+            return (float)(ticksRemaining <= 0 ? 0 : Math.Ceiling(ticksRemaining / GenDate.TicksPerHour));
         }
     }
 }
